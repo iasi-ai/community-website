58513c41d85d70c5a115d51d88d426baf5ee19e4
IAȘI AI community <eugen@iasi.ai>
2023-04-06 18:32:01 +0300

    Updated buffer

--- assets/js/app.js
+++ assets/js/app.js
@@ -358,7 +358,7 @@ const IasiAIApp = (() => {
                         `${middlePosition + rightPosition++}` : `${leftPosition++}`;
 
                     containerHeight += (ordering % 2 === 0) ?
-                        590 : 0;
+                        610 : 0;
 
                     ordering++;
                 });

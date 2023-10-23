125b765de9ee1218798976e9f96b803ab7e74d1d
IAȘI AI community <eugen@iasi.ai>
2023-10-23 20:05:21 +0300

    Normalized meetup cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -358,7 +358,7 @@ const IasiAIApp = (() => {
                         `${middlePosition + rightPosition++}` : `${leftPosition++}`;
 
                     containerHeight += (ordering % 2 === 0) ?
-                        20 : 0;
+                        520 : 0;
 
                     ordering++;
                 });
